ef9b1998927398234c3ee18da41673ea97d5aa6d
Sourabh-Bhakar5228 <golubhakar5228@gmail.com>
2025-11-26 13:14:16 +0530

    update resume

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/sourabh_updated.pdf";
+import pdf from "../../Assets/sourabh updated.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
